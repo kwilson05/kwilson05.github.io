f1eb20e7736326c35dfd5c25f075b7bb10a720be
kasozi wilson <kasoziwilson@kasozis-MBP.lan>
2022-01-19 22:16:24 -0500

    serve css

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,16 @@
 module.exports = (eleventyConfig) => {
-
-    eleventyConfig.addLayoutAlias('home', 'layouts/home.html');
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addLayoutAlias("home", "layouts/home.html");
     eleventyConfig.setLiquidOptions({
-        root: [
-          '_includes',
-          '.'
-        ]
-      });
+        root: ["_includes", "."],
+    });
 
     return {
         dir: {
             input: "./",
             output: "./_site",
         },
+        passthroughFileCopy: true,
     };
 };
