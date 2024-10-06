f87462255d4a8b354bd45c9387b276ec26278e3c
Kasozi Wilson <kwilson05@users.noreply.github.com>
2024-10-06 00:20:55 -0400

    Astro build (#16)
    * astro install and prettier
    
    * skeleton header
    
    * stupid simple template
    
    * reset css
    
    * skeleton blog
    
    * some padding
    
    * need to get accessible colors
    
    * about me paragraph 1
    
    * good enough for now
    
    * remove space from external link
    
    * focus outline
    
    * responsiveness
    
    * centered layout
    
    * silly kid trix are for kids
    
    * pottery link
    
    * 404
    
    * nouns
    
    * no nav menu yet
    
    * youtube
    
    * period
    
    * readme
    
    * space

---
+++ src/extenalLinks.js
@@ -0,0 +1,6 @@
+export const POTTERY_IG_URL = 'https://www.instagram.com/potsbykasozi/';
+export const META_METRICS_URL = 'https://metametricsinc.com/about-us/';
+export const INGEN_SOFTWARE_URL = 'http://ingensoftware.com/MainSite/';
+export const ESYSTEM_INC_URL = 'https://www.esystems-inc.com/';
+export const GITHUB_URL = 'https://github.com/kwilson05';
+export const YOUTUBE_URL = 'https://www.youtube.com/watch?v=10BTAiFP0Rg';
