437713b6326fb4aa49994bd312deba4826d1d557
Kasozi Wilson <kwilson05@users.noreply.github.com>
2024-10-06 00:12:28 -0400

    youtube

--- src/extenalLinks.js
+++ src/extenalLinks.js
@@ -3,3 +3,4 @@ export const META_METRICS_URL = 'https://metametricsinc.com/about-us/';
 export const INGEN_SOFTWARE_URL = 'http://ingensoftware.com/MainSite/';
 export const ESYSTEM_INC_URL = 'https://www.esystems-inc.com/';
 export const GITHUB_URL = 'https://github.com/kwilson05';
+export const YOUTUBE_URL = 'https://www.youtube.com/watch?v=10BTAiFP0Rg';
