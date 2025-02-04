12afd308b517ba30320b437769d463416f771fed
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-02-03 23:47:18 -0500

    generate first dynamic blog entry

---
+++ blog/Welcome.md
@@ -0,0 +1,7 @@
+---
+title: 'Hello World'
+draft: false
+publishedDate: 2024-02-03
+---
+
+Today we generated our first blog post
---
+++ src/content.config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+//import loaders
+import { glob } from 'astro/loaders';
+
+const blogCollection = defineCollection({
+    loader: glob({ pattern: '**/*.md', base: './blog' }),
+    schema: z.object({
+        title: z.string(),
+        publishedDate: z.date(),
+        draft: z.boolean().default(false),
+    }),
+});
+
+export const collections = { blog: blogCollection };
