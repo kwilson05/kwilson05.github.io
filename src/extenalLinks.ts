0474a413a9b26cd1a676fb9afa51e7935997ebd1
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-03-12 17:38:35 -0400

    add image and home page og:description

--- src/extenalLinks.ts
+++ src/extenalLinks.ts
@@ -1,6 +1,4 @@
 export const POTTERY_IG_URL = 'https://www.instagram.com/potsbykasozi/';
-export const META_METRICS_URL = 'https://metametricsinc.com/about-us/';
-export const INGEN_SOFTWARE_URL = 'http://ingensoftware.com/MainSite/';
-export const ESYSTEM_INC_URL = 'https://www.esystems-inc.com/';
 export const GITHUB_URL = 'https://github.com/kwilson05';
 export const YOUTUBE_URL = 'https://www.youtube.com/watch?v=10BTAiFP0Rg';
+export const LINKEDIN = 'https://www.linkedin.com/in/kasozi-wilson-42042632a/';
