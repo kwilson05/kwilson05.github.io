da38cd54692145188b4d970454790c2e26b3194f
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-04-03 23:28:49 -0400

    compress and size og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,11 @@ const { VITE_BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd());
 // https://astro.build/config
 export default defineConfig({
     site: VITE_BASE_URL ? VITE_BASE_URL : 'http://localhost:4321/',
+
     redirects: {
         '/blog': '/blog/page/1',
+    },
+    server: {
+        allowedHosts: true,
     },
 });
