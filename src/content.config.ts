36e254b38295cb32eede3bc812fb3b5e1a8c8bac
Kasozi Wilson <kwilson05@users.noreply.github.com>
2025-04-15 01:04:40 -0400

    restore file

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,20 +3,6 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
-    /* loader: async () => {
-        const blogPosts = Object.values(
-            import.meta.glob('/blog/*.md', { eager: true })
-        );
-
-        return blogPosts.map((blogPost: any) => {
-            return {
-                readingTimeMinutes: readingTime(blogPost.rawContent()).minutes,
-                ...blogPost.frontmatter,
-                publishedDate: new Date(blogPost.frontmatter.publishedDate),
-                ...blogPost,
-            };
-        });
-    },*/
     loader: glob({ pattern: '**/*.md', base: './blog' }),
     schema: z.object({
         title: z.string(),
