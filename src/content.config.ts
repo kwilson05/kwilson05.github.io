db431ebdfb3c9ea7fba1bf5307b2c892b879cab2
Kasozi Wilson <kwilson05@users.noreply.github.com>
2026-02-21 22:49:49 -0500

    add rss feed (#37)

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,14 +1,12 @@
 import { defineCollection, z } from 'astro:content';
+import { rssSchema } from '@astrojs/rss';
 
 import { glob } from 'astro/loaders';
 
 const blogCollection = defineCollection({
     loader: glob({ pattern: '**/*.md', base: './blog' }),
-    schema: z.object({
-        title: z.string(),
-        publishedDate: z.date(),
+    schema: rssSchema.extend({
         draft: z.boolean().default(false),
-        headline: z.string(),
     }),
 });
 
